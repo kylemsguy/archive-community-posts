d90c697d54c976f214e5d11914ef43fbbac027db
Kyle Zhou <kylezhou2002@hotmail.com>
2023-04-27 13:41:56 +0800

    Now deduplicates the extracted links

--- extract_post_urls.py
+++ extract_post_urls.py
@@ -2,6 +2,8 @@ import argparse
 
 from bs4 import BeautifulSoup
 
+# Honestly this could probably be done with pure regex... but I already used beautifulsoup and it works...
+
 def get_arg_parser():
     parser = argparse.ArgumentParser(description='Extracts community post URLs from a dump of the community post tab.')
     parser.add_argument('communitypage',
@@ -32,10 +34,26 @@ def write_full_links(outfilepath, links, baseurl="https://www.youtube.com"):
         for l in links:
             outfile.write(f"{baseurl}{l}\n")
 
+def dedupe(urls):
+    """
+    Dedupes the list of URLs while preserving order
+
+    Returns the result as a new list.
+    """
+    url_set = set()
+    newurl_list = []
+    for url in urls:
+        if url not in url_set:
+            url_set.add(url)
+            newurl_list.append(url)
+
+    return newurl_list
+
 if __name__ == "__main__":
     args = get_arg_parser().parse_args()
     tag_soup = read_tag_soup(args.communitypage)
-    links = parse_links(tag_soup)
+    links_raw = parse_links(tag_soup)
+    links = dedupe(links_raw)
     if args.outfile:
         write_full_links(args.outfile, links)
     else:
